72da0a9567630ff7f6dedea961726ce4287dc867
myoaoo <ssjia.arron@gmail.com>
2024-09-03 08:45:37 +0800

    up

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,7 +4,7 @@
       v-if="settings.showPlaylistsByAppleMusic !== false"
       class="index-row first-row"
     >
-      <div class="title">我的歌单</div>
+      <div class="title"> by Apple Music </div>
       <CoverRow
         :type="'playlist'"
         :items="byAppleMusic"
